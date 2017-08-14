d9c9c2ac4efa551c266d4701ee2a8a897d2a08ba
xschen <xs0040@gmail.com>
2017-08-14 09:06:45 +0800

    convnet for cifar10 (simple)

---
+++ pydl/keras/convnet/convnet_cifar10_simple.py
@@ -0,0 +1,77 @@
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import Adam, RMSprop
+
+import matplotlib.pyplot as plt
+
+IMG_CHANNELS = 10
+IMG_ROWS = 32
+IMG_COLS = 32
+
+BATCH_SIZE = 128
+NB_EPOCH = 20
+NB_CLASSES = 10
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+OPTIM = RMSprop()
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=3, padding="same", input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIM, metrics=['accuracy'])
+history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH, verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+print('Testing...')
+score = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
+print('\nTest score:', score[0])
+print('Test accuracy:', score[1])
+
+# save model
+model_json = model.to_json()
+open('/tmp/cifar10_architecture.json','w').write(model_json)
+model.save_weights('/tmp/cifar10_weights.h5', overwrite=True)
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['val_acc']);
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize  history for loss
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
