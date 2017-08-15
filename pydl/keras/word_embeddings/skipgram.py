3efa2510da50478cd9c223ebb1693f4c8a00879a
xschen <xs0040@gmail.com>
2017-08-15 19:00:13 +0800

    skipgrams

--- pydl/keras/word_embeddings/skipgram.py
+++ pydl/keras/word_embeddings/skipgram.py
@@ -9,14 +9,19 @@ EMBED_SIZE = 300
 word_model = Sequential()
 word_model.add(Embedding(input_dim=VOCAB_SIZE, output_dim=EMBED_SIZE, embeddings_initializer='glorot_uniform',
                          input_length=1))
-word_model.add(Reshape(EMBED_SIZE, ))
+word_model.add(Reshape((EMBED_SIZE, )))
 
 context_model = Sequential()
 context_model.add(Embedding(input_dim=VOCAB_SIZE, output_dim=EMBED_SIZE, embeddings_initializer='glorot_uniform',
                             input_length=1))
-context_model.add(Reshape(EMBED_SIZE, ))
+context_model.add(Reshape((EMBED_SIZE, )))
 
 model = Sequential()
 model.add(Merge([word_model, context_model], mode='dot'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer='adam')
+
+merge_layer = model.layers[0]
+word_model = merge_layer.layers[0]
+word_embed_layer = word_model.layers[0]
+weights = word_embed_layer.get_weights()[0]
